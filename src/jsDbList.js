7071949c39f270b58831167e09039955dbe5e48d
Gaetano Lazzo <gaetano.lazzo@gmail.com>
2015-10-01 14:39:23 +0200

    firsr commit

---
+++ src/jsDbList.js
@@ -0,0 +1,320 @@
+/**
+ * DB list
+ */
+/*jslint node: true */
+'use strict';
+/* globals Connection */
+
+var
+  EncryptedFile = require('jsEncryptedFile'),
+  Deferred = require("JQDeferred"),
+  DataAccess = require("jsDataAccess").DataAccess,
+  _ = require('lodash');
+
+
+/**
+ * Maintains a list of db connection information, each identified by a dbCode
+ * @module dbList
+ */
+
+
+
+
+/**
+ * @class dbDescriptor
+ * A dbDescriptor takes track of the structure of a database. It doesn't manage different schemas.
+ * The structure of a table is described with a TableDescriptor
+ * module dbDescriptor
+ */
+
+
+
+/**
+ * @class TableDescriptor
+ * The structure of a table is described with a TableDescriptor.
+ * A TableDescriptor is an object having those properties:
+ * {string} xtype:      T for  tables, V for Views
+ * {string} name:       table or view name
+ * {ColumnDescriptor[]} columns
+ *
+ */
+
+/**
+ * @class ColumnDescriptor
+ * An object describing a column of a table. It is required to have the following fields:
+ *  {string} name        - field name
+ *  {string} type        - db type
+ *  {number} max_length  - size of field in bytes
+ *  {number} precision   - n. of integer digits managed
+ *  {number} scale       - n. of decimal digits
+ *  {boolean} is_nullable - true if it can be null
+ *  {boolean} pk          - true if it is primary key
+ */
+
+
+
+var dbListFile, dbList;
+
+/**
+ * Initializes dbList
+ * @method init
+ * @param options
+ * @param {string} options.fileName Name of the clean file to encrypt
+ * @param {string} options.encryptedFileName name of the file to be created
+ * @param {bool} options.encrypt true if the file has to be encrypted
+ * @param {bool} options.decrypt true if the file has to be decrypted
+ * @param {object} [options.secret] object contaning key,iv,pwd to replace the config
+
+ */
+function init(options){
+  dbListFile = new EncryptedFile(options);
+  dbList = dbListFile.read();
+}
+/**
+ * Creates a DbDescriptor
+ * @class DbDescriptor
+ * @param {Connection} sqlConn
+ * @constructor
+ */
+function DbDescriptor(sqlConn) {
+
+  /**
+   * @private
+   * Collection of stored TableDescriptor
+   * @property tables
+   * @type Array[DataTables]
+   */
+  this.tables = {};
+  this.sqlConn = sqlConn;
+}
+
+DbDescriptor.prototype = {
+  constructor: DbDescriptor
+};
+
+/**
+ * Get/Set the structure of a table in a JQuery fashioned style
+ * @method table
+ * @class DbDescriptor
+ * @param {string} tableName
+ * @param {TableDescriptor} [tableDescriptor]
+ * @returns {TableDescriptor} promise to a TableDescriptor or undefined if it was a Set
+ */
+DbDescriptor.prototype.table = function (tableName, tableDescriptor) {
+  var def = Deferred(),
+    that = this;
+  if (tableDescriptor === undefined) {
+    if (this.tables[tableName]) {
+      def.resolve(this.tables[tableName]);
+      return def.promise();
+    }
+    this.sqlConn
+      .tableDescriptor(tableName)
+      .done(function (results) {
+        that.tables[tableName] = new TableDescriptor(results.tableName, results.xtype,
+          results.isDbo, results.columns);
+        def.resolve(that.tables[tableName]);
+      })
+      .fail(function (err) {
+        def.reject(err);
+      });
+    return def.promise();
+  }
+  this.tables[tableName] = tableDescriptor;
+};
+
+/**
+ * Clears the information stored about a table
+ * @method forgetTable
+ * @class DbDescriptor
+ * @param {string} tableName
+ * @returns {*}
+ */
+DbDescriptor.prototype.forgetTable = function (tableName) {
+  delete this.tables[tableName];
+};
+
+
+/**
+ * @private
+ * creates a TableDescriptor
+ * @method TableDescriptor
+ * @class TableDescriptor
+ * @private
+ * @constructor
+ * @param {string} name
+ * @param {'T'|'V'} xtype  T for tables, V for views
+ * @param {boolean} isDbo true if is DBO table/view
+ * @param {ColumnDescriptor[]} columns
+ */
+function TableDescriptor(name, xtype, isDbo, columns) {
+  this.name = name;
+  this.xtype = xtype;
+  this.dbo = isDbo;
+  this.columns = columns;
+}
+
+
+TableDescriptor.prototype = {
+  constructor: TableDescriptor
+};
+
+/**
+ * gets a column descriptor given the column name
+ * @method column
+ * @class TableDescriptor
+ * @param {string} columnName
+ * @returns {ColumnDescriptor}
+ */
+TableDescriptor.prototype.column = function (columnName) {
+  return _.find(this.columns, {'name': columnName});
+};
+
+/**
+ * gets an array of all primary key column names
+ * @method getKey
+ * @class TableDescriptor
+ * @returns {array}
+ */
+TableDescriptor.prototype.getKey = function () {
+  return _.pluck(_.where(this.columns, {pk: true}), 'name');
+};
+
+/**
+ * @private
+ * @property allDescriptor
+ * @type: Hash of DbDescriptor
+ */
+var allDescriptors = {};
+
+/**
+ * @method getDescriptor
+ * @param {string} dbCode
+ * @returns {DbDescriptor}
+ */
+function getDescriptor(dbCode) {
+  if (allDescriptors.hasOwnProperty(dbCode)) {
+    return allDescriptors[dbCode];
+  }
+  allDescriptors[dbCode] = new DbDescriptor(getConnection(dbCode));
+  return allDescriptors[dbCode];
+}
+
+
+/**
+ * gets a Connection eventually taking it from a pool, at the moment it simply returns a new Connection
+ * @param {string} dbCode
+ * @returns {Connection}
+ */
+function getConnection(dbCode) {
+  var options = getDbInfo(dbCode);
+  if (options) {
+    options.dbCode = dbCode;
+    var Connection = require(options.sqlModule).Connection;
+    if (Connection) {
+      return new Connection(options);
+    }
+  }
+  return undefined;
+}
+
+/**
+ * Gets  a promis to a DataAccess
+ * @param {string} dbCode
+ * @returns {*}
+ */
+function getDataAccess(dbCode) {
+  var q = Deferred(),
+    sqlConn = this.getConnection(dbCode);
+  new DataAccess({
+    sqlConn: sqlConn,
+    errCallBack: function (err) {
+      q.reject(err);
+    },
+    doneCallBack: function (DA) {
+      //console.log('resolved:'.DA);
+      q.resolve(DA);
+    }
+  });
+
+
+  return q.promise();
+}
+
+/**
+ * Get information about a database
+ * @method getDbInfo
+ * @param {string} dbCode
+ * @returns {driver,useTrustedConnection,user,pwd,database,defaultSchema,connectionString} same data as that
+ *  required for sqlConnection constructor:
+ * {string} [driver='SQL Server Native Client 11.0'] Driver name
+ * {string} [useTrustedConnection=true] is assumed true if no user name is provided
+ * {string} [user] user name for connecting to db
+ * {string} [pwd] user password for connecting to db
+ * {string} [database] database name
+ * {string} [defaultSchema=options.user ||'DBO'] default schema associated with user name
+ * {string} [connectionString] connection string to connect (can be used instead of all previous listed)
+ * {string} sqlModule module name to user for getting connection
+
+ */
+function getDbInfo(dbCode) {
+  if (dbList.hasOwnProperty(dbCode)) {
+    return dbList[dbCode];
+  }
+  return undefined;
+}
+
+
+
+/**
+ * sets information about a database
+ * @method setDbInfo
+ * @param {string}dbCode
+ * @param {object} dbData
+ */
+function setDbInfo(dbCode, dbData) {
+  dbList[dbCode] = dbData;
+  dbListFile.write(dbList);
+}
+
+/**
+ * Deletes a Db from the list
+ * @method delDbInfo
+ * @param {string} dbCode
+ * @returns {*}
+ */
+function delDbInfo(dbCode) {
+  if (dbList.hasOwnProperty(dbCode)) {
+    delete dbList[dbCode];
+    dbListFile.write(dbList);
+  }
+}
+
+
+/**
+ * Check if a dbCode is present in the list
+ * @method existsDbInfo
+ * @param {string} dbCode
+ * @returns {boolean}
+ */
+function existsDbInfo(dbCode) {
+  return dbList.hasOwnProperty(dbCode);
+}
+
+
+module.exports = {
+  init: init,
+
+  getDbInfo: getDbInfo,
+  setDbInfo:setDbInfo,
+  delDbInfo:delDbInfo,
+  existsDbInfo: existsDbInfo,
+
+  getConnection: getConnection,
+  getDataAccess: getDataAccess,
+
+  DbDescriptor: DbDescriptor,
+  TableDescriptor: TableDescriptor,
+  getDescriptor: getDescriptor
+};
+
---
+++ test/spec/jsDbListSpec.js
@@ -0,0 +1,240 @@
+/*globals describe,beforeEach,it,expect,jasmine,spyOn,afterEach,xit*/
+'use strict';
+
+var DataAccess = require('jsDataAccess');
+var sqlServerDriver = require('jsSqlServerDriver');
+
+var Deferred = require("JQDeferred");
+var dbList = require('../../src/jsDbList');
+var DbDescriptor = dbList.DbDescriptor;
+var fs= require('fs');
+
+var $dq = require('jsdataquery');
+var _ = require('lodash');
+
+/**
+ * *****************************************************************************************
+ * VERY IMPORTANT VERY IMPORTANT VERY IMPORTANT VERY IMPORTANT VERY IMPORTANT VERY IMPORTANT
+ * *****************************************************************************************
+ * It's necessary, before start running the test, to create a file templated like:
+ *  { "server": "db server address",
+ *    "dbName": "database name",  //this must be an EMPTY database
+ *    "user": "db user",
+ *    "pwd": "db password"
+ *  }
+ */
+//PUT THE  FILENAME OF YOUR FILE HERE:
+var configName = 'D:/gitrepo/jsDBList/test/db.json';
+
+var dbConfig = JSON.parse(fs.readFileSync(configName).toString());
+
+/**
+ * setup the dbList module
+ */
+dbList.init({ encrypt: false,
+                decrypt: false,
+                encryptedFileName: 'test/dbList.bin'
+            });
+
+var good = {
+        server: dbConfig.server,
+        useTrustedConnection: false,
+        user: dbConfig.user,
+        pwd: dbConfig.pwd,
+        database: dbConfig.dbName,
+        sqlModule: 'jsSqlServerDriver'
+    };
+
+
+describe('setup dataBase', function () {
+    var sqlConn;
+    beforeEach(function (done) {
+        dbList.setDbInfo('test',good);
+        sqlConn = dbList.getConnection('test');
+        sqlConn.open().
+            done(function () {
+                done();
+            });
+    }, 10000);
+
+    afterEach(function () {
+        if (sqlConn) {
+            sqlConn.destroy();
+        }
+        sqlConn = null;
+    });
+
+
+    it('should run the setup script', function (done) {
+        sqlConn.run(fs.readFileSync('test/setup.sql').toString())
+            .done(function () {
+                expect(true).toBeTruthy();
+                done();
+            })
+            .fail(function (res) {
+                expect(res).toBeUndefined();
+                done();
+            });
+    }, 30000);
+
+});
+
+describe('dbDescriptor ', function () {
+    var sqlConn,
+        dbDescr;
+
+
+    beforeEach(function (done) {
+        sqlConn = dbList.getConnection('test');
+        dbDescr = new DbDescriptor(sqlConn);
+        sqlConn.open().
+            done(function () {
+                done();
+            });
+    });
+
+    afterEach(function () {
+        if (sqlConn) {
+            sqlConn.destroy();
+        }
+        sqlConn = null;
+    });
+
+
+    describe('table descriptor', function () {
+        it('tableDescriptor should be a method', function () {
+            expect(sqlConn.tableDescriptor).toEqual(jasmine.any(Function));
+        });
+
+        it('tableDescriptor should return an object when table exists', function (done) {
+            sqlConn.tableDescriptor('customer')
+                .done(function (p) {
+                    expect(p).toEqual(jasmine.any(Object));
+                    done();
+                })
+                .fail(function (err) {
+                    expect(err).toBeUndefined();
+                    done();
+                });
+        });
+
+
+        it('tableDescriptor should fail when table does not exist', function (done) {
+            sqlConn.tableDescriptor('customer_no')
+                .done(function (p) {
+                    expect(p).toBeUndefined();
+                    done();
+                })
+                .fail(function (err) {
+                    expect(err).toContain('does not exist');
+                    done();
+                });
+
+        });
+
+
+        it('tableDescriptor should return an object with columns array, name and xtype', function (done) {
+            dbDescr.table('customer')
+                .done(function (p) {
+                    expect(p.name).toEqual('customer');
+                    expect(p.xtype).toEqual('T');
+                    expect(p.columns).toEqual(jasmine.any(Array));
+                    done();
+                })
+                .fail(function (err) {
+                    expect(err).toBeUndefined();
+                    done();
+                });
+        });
+
+        it('tableDescriptor should have a key', function (done) {
+            dbDescr.table('customer')
+                .done(function (p) {
+                    expect(p.getKey).toEqual(jasmine.any(Function));
+                    expect(p.getKey()).toEqual(jasmine.any(Array));
+                    expect(p.getKey()[0]).toEqual('idcustomer');
+                    done();
+                })
+                .fail(function (err) {
+                    expect(err).toBeUndefined();
+                    done();
+                });
+        });
+
+        it('A view should not have a key', function (done) {
+            dbDescr.table('customerview')
+                .done(function (p) {
+                    expect(p.getKey).toEqual(jasmine.any(Function));
+                    expect(p.getKey()).toEqual(jasmine.any(Array));
+                    expect(p.getKey().length).toEqual(0);
+                    done();
+                })
+                .fail(function (err) {
+                    expect(err).toBeUndefined();
+                    done();
+                });
+        });
+
+    });
+
+
+});
+
+
+describe('dbList', function () {
+
+    it('dbList should be defined', function () {
+        expect(dbList).toBeDefined();
+    });
+
+    it('dbList should be a object', function () {
+        expect(dbList).toEqual(jasmine.any(Object));
+    });
+
+
+    it('dbList() should define getDbInfo', function () {
+        expect(dbList.getDbInfo).toBeDefined();
+    });
+
+    it('dbList() should define setDbInfo', function () {
+        expect(dbList.setDbInfo).toBeDefined();
+    });
+});
+
+
+
+describe('destroy dataBase', function () {
+    var sqlConn;
+    beforeEach(function (done) {
+        dbList.setDbInfo('test',good);
+        sqlConn = dbList.getConnection('test');
+        sqlConn.open().
+            done(function () {
+                done();
+            });
+    }, 10000);
+
+    afterEach(function () {
+        dbList.delDbInfo('test');
+        if (sqlConn) {
+            sqlConn.destroy();
+        }
+        sqlConn = null;
+        if (fs.existsSync('test/dbList.bin')) {
+            fs.unlinkSync('test/dbList.bin');
+        };
+    });
+
+    it('should run the destroy script', function (done) {
+        sqlConn.run(fs.readFileSync('test/destroy.sql').toString())
+            .done(function () {
+                expect(true).toBeTruthy();
+                done();
+            })
+            .fail(function (res) {
+                expect(res).toBeUndefined();
+                done();
+            });
+    }, 30000);
+
+});
